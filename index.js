de2ea621b6b72de635e515d35bf8acd1ceaf3270
Suzanne Pach <51491480+suzannepach@users.noreply.github.com>
2023-06-15 13:23:12 +0200

    Get data from json file instead of using hard coded html

---
+++ index.js
@@ -0,0 +1,56 @@
+const reactionCat = document.getElementById("reaction-cat");
+const reactionScore = document.getElementById("reaction-score");
+const reactionImg = document.getElementById("image-reaction");
+
+const memoryCat = document.getElementById("memory-cat");
+const memoryScore = document.getElementById("memory-score");
+const memoryImg = document.getElementById("image-memory");
+
+const verbalCat = document.getElementById("verbal-cat");
+const verbalScore = document.getElementById("verbal-score");
+const verbalImg = document.getElementById("image-verbal");
+
+const visualCat = document.getElementById("visual-cat");
+const visualScore = document.getElementById("visual-score");
+const visualImg = document.getElementById("image-visual");
+
+// reactionCat.textContent = "Reaction";
+// reactionScore.textContent = "80";
+// reactionImg.innerHTML = 
+//     `<source srcset="assets/images/icon-reaction.svg" type="image/svg">
+//     <img src="assets/images/icon-reaction.svg" alt="icon reaction">`
+
+// How to get data from a JSON file: https://www.freecodecamp.org/news/how-to-read-json-file-in-javascript/ 
+// It only works when the json file is stored in a server (local or remote) not when it is stored locally
+// That is why I fetch the JSON file from my gh-pages even when working local. I have to use the raw
+// file, not the normal gh file.
+// I don't really understand why I don't need to use parse() to get the json content.
+fetch('https://raw.githubusercontent.com/suzannepach/results-summary-component/main/data.json')
+    .then((response) => response.json())
+    .then((json) => { 
+        reactionCat.textContent = json[0].category;
+        reactionScore.textContent = json[0].score;
+        reactionImg.innerHTML = 
+        `<source srcset=${json[0].icon} type="image/svg">
+        <img src=${json[0].icon} alt="icon reaction">`
+
+        memoryCat.textContent = json[1].category;
+        memoryScore.textContent = json[1].score;
+        memoryImg.innerHTML = 
+        `<source srcset=${json[1].icon} type="image/svg">
+        <img src=${json[1].icon} alt="icon reaction">`
+
+        verbalCat.textContent = json[2].category;
+        verbalScore.textContent = json[2].score;
+        verbalImg.innerHTML = 
+        `<source srcset=${json[2].icon} type="image/svg">
+        <img src=${json[2].icon} alt="icon reaction">`
+
+        visualCat.textContent = json[3].category;
+        visualScore.textContent = json[3].score;
+        visualImg.innerHTML = 
+        `<source srcset=${json[3].icon} type="image/svg">
+        <img src=${json[3].icon} alt="icon reaction">`
+    });
+
+
